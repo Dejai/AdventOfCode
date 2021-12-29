1fd94d1f53eb3948d1263ee10ca46d77c8093445
Derrick Fyfield <dfyfiel1@jhu.edu>
2021-12-28 23:17:02 -0500

    prod day 18

---
+++ 2021/18/helper.py
@@ -0,0 +1,231 @@
+import math
+from collections import deque
+
+
+DEBUG = False
+
+class Pair:
+
+    def __init__(self, array=None, parent=None):
+
+        self.parent = parent
+        self.left = None
+        self.right = None
+
+        if array is not None:
+            self.createPair(array)
+
+    def __repr__(self):
+        return repr([self.left,self.right]).replace(" ", "")
+    
+
+    # Create a pair from "scratch" (i.e. with provided array values)
+    def createPair(self, array):
+        left = array[0]
+        right = array[1]
+
+        self.left = Pair(left, self) if type(left) != int else left
+        self.right = Pair(right, self) if type(right) != int else right
+
+    # Reduce a number
+    def reduce(self):
+
+        if DEBUG:
+            print("Reducing: %s" % str(self))
+
+        while True:
+
+            actionTaken = False
+
+            # Scan the tree to find actionable items
+            nodeMap,explodable,splittable = self.scanTree()
+
+            # EXPLODE (if applicable)
+            if len(explodable) == 1:
+                node = explodable.pop()
+                node.explode(nodeMap)
+                actionTaken = True
+
+            # SPLIT (if applicable)
+            elif len(splittable) == 1:
+                node = splittable.pop()
+                node.split()
+                actionTaken = True
+
+            # Stop reducing if no actions were taken
+            if not actionTaken:
+                break
+        
+        if DEBUG:
+            print("Result: %s" %str(self))
+
+
+    # Use Depth First search to scan the tree and get details on Explodable and Splittable nodes
+    def scanTree(self):
+
+        '''
+            Each tuple in the queue contains:
+                node = the node in the graph (could be pair or digit)
+                parent = the parent node of the current node
+                depth = the depth away from root
+        '''
+
+        # Setup queue, with root node first
+        theQueue = deque()        
+        theQueue.append( (self, None, 0) )
+
+        digits = []
+        neighborMap = {}
+        parents = []
+        sides = []
+        explodable = []
+        splittable = []
+        checking = []
+        
+        # Use DFS to get the left/right digits based on left-side priority of nodes
+        while theQueue:
+
+            node,parent,depth = theQueue.pop()
+            nodeType = type(node)
+
+
+            if nodeType == Pair and node not in neighborMap:
+                neighborMap[node] = {"left":None, "right": None}
+
+
+            # This is a "leaf", and nothing left to expand
+            if nodeType == int:
+                
+                # Check if previous digit/parent was added:
+                if len(parents) > 0 and parents[-1] != parent:
+                    prevNode = parents[-1]
+                    neighborMap[parent]["left"] = prevNode
+                    neighborMap[prevNode]["right"] = parent
+                
+                digits.append(node)
+                parents.append(parent)
+
+                # Add splits
+                if node >= 10 and len(splittable) == 0:
+                    splittable.append(parent)
+
+                continue
+
+            elif nodeType == Pair and depth >= 4 and node.isAllDigits() and len(explodable) == 0:
+            
+                explodable.append(node)
+
+            
+            # Adding right first, so it's checked last
+            theQueue.append( (node.right, node, depth+1) )
+
+            # Adding left last, so it's popped first
+            theQueue.append( (node.left, node, depth+1) )
+
+        return neighborMap,explodable,splittable
+
+
+    # Explode a pair if it is too deep
+    def explode(self,nodeMap):
+
+        if DEBUG:
+            print("\t\tEXPLODING: %s ; parent=%s" % ( str(self), str(self.parent)))
+            print("\n")
+
+        # Get left node
+        left = nodeMap[self]["left"]
+        if left is not None:
+            left.increaseDigit(self.left, "left")
+
+        # Get right node
+        right = nodeMap[self]["right"]
+        if right is not None:
+            right.increaseDigit(self.right, "right")
+
+        # Set the pair (right) to a zero (if applicable)                
+        if self.parent.right == self:
+            self.parent.right = 0
+
+        # Set the pair (left) to a zero (if applicable)                
+        if self.parent.left == self:
+            self.parent.left = 0
+        
+
+    # Split a given node's higher value (prioritizing left)
+    def split(self):
+
+        value = None
+        side = None
+
+        if type(self.left) == int and self.left >= 10:
+            value = self.left
+            side = "left"
+        elif type(self.right) == int and self.right >= 10:
+            value = self.right
+            side = "right"
+
+        if DEBUG:
+            print("\t\tSPLITTING: %s (value=%d)" %(str(self), value))
+            print("\n")
+
+        # Create the split pair
+        if value is not None and side is not None:
+            # Setup new split pair
+            newLeft = math.floor(value/2)
+            newRight = math.ceil(value/2)
+            newPair = Pair( [newLeft, newRight], self )
+
+            # Reassign the split pair, and account for explode if needed
+            if side == "left":
+                self.left = newPair
+
+            elif side == "right":
+                self.right = newPair
+        
+
+
+    # Get magnitude of the current number
+    def getMagnitude(self):
+
+        leftVal = 0
+        rightVal = 0
+
+        if type(self.left) == Pair:
+            leftVal = self.left.getMagnitude()
+        else: 
+            leftVal = self.left
+
+        if type(self.right) == Pair:
+            rightVal = self.right.getMagnitude()
+        else: 
+            rightVal = self.right
+
+        
+        return (3*leftVal) + (2*rightVal)
+
+
+    # HELPER: Increase the digit of the respective node by the given value
+    def increaseDigit(self, value, comingFrom):
+
+        # Prioritize left side integers if expanding from right
+        if comingFrom == "right":
+            if type(self.left) == int:
+                self.left += value
+            elif type(self.right) == int:
+                self.right += value
+
+        # Otherwise, prioritize right side value if expanding from left
+        elif comingFrom == "left":
+            if type(self.right) == int:
+                self.right += value
+            elif type(self.left) == int:
+                self.left += value
+
+
+    # HELPER METHOD: Returns if the current pair contains all digits
+    def isAllDigits(self):
+        isDigits = type(self.left) == int and type(self.right) == int
+        return isDigits
+
+
+
---
+++ 2021/18/script.py
@@ -0,0 +1,107 @@
+from math import exp
+import shared
+from helper import Pair
+'''
+***************************************************************
+    Input
+***************************************************************
+'''
+
+inputList = shared.getFileContentAsList("./input.txt")
+
+
+'''
+***************************************************************
+   Functions
+***************************************************************
+'''
+
+# Add 2 numbers
+def add2Numbers(number1, number2):
+
+    resultingPair = Pair()
+
+    number1.parent = resultingPair
+    number2.parent = resultingPair
+
+    resultingPair.left = number1
+    resultingPair.right = number2
+
+    return resultingPair
+
+
+# Compare the magnitudes of 2 numbers
+def compareMagnitudes(number1, number2):
+
+    added = add2Numbers( Pair(eval(number1)), Pair(eval(number2)) )
+    added.reduce()
+
+    magnitude = added.getMagnitude()
+
+    return magnitude
+
+
+
+# Test an addition (confirming the end results)
+def testAddition(number1, number2, expected):
+
+    added = add2Numbers( Pair(eval(number1)), Pair(eval(number2)) )
+    added.reduce()
+    print(added.getMagnitude())
+
+    results = str(added)
+
+    print("Expected:\t %s"  % str(expected))
+    print("What I got:\t %s"  % results)
+    print("-"*50)
+    print( str(expected) == results )
+    print("-"*50)
+
+
+
+'''
+***************************************************************
+    Run
+***************************************************************
+'''
+
+# Part 1
+snailfishNums = inputList[:]
+allCombined = Pair(eval(snailfishNums[0]))
+largestMagnitude = 0
+
+for idx in range(0, len(snailfishNums)):
+
+    currNumber = snailfishNums[idx]
+
+    for idx2 in range(0, len(snailfishNums)):
+
+        # No need to add a number to itself
+        if idx2 == idx:
+            continue
+
+        nthNumber = snailfishNums[idx2]
+
+        # For Part 1
+        if idx == 0 and idx2 > 0:
+            nextCombo = add2Numbers(allCombined, Pair(eval(nthNumber)) )
+            nextCombo.reduce()
+            allCombined = nextCombo
+
+        # Fort Part 2
+        comboMagnitude = compareMagnitudes(currNumber, nthNumber)
+        largestMagnitude = max(largestMagnitude, comboMagnitude)
+    
+# Part 1
+shared.printAnswer(allCombined.getMagnitude())
+
+# Part 2
+shared.printAnswer(largestMagnitude,2)
+
+# TESTING:
+#testAddition("[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]","[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]","[[[[7,8],[6,6]],[[6,0],[7,7]]],[[[7,8],[8,8]],[[7,9],[0,6]]]]")
+#testAddition("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]", "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]", "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]")
+#testAddition("[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]","[7,[5,[[3,8],[1,4]]]]","[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]")
+#testReduction("[[[[7, 7], [7, 8]], [[9, 5], [8, 7]]], [[[0, 7], [15, 10]], [8, [9, 0]]]]")
+#testReduction("[[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]]")
+# testAddition("[[[[1,1],[2,2]],[3,3]],[4,4]]")
