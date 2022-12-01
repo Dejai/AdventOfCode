cbdb3516e6942dd509171e8bc5f5792ad1edbbb4
Derrick Fyfield <dancinglion.dev@gmail.com>
2022-12-01 17:03:23 -0500

    prod 2022 day 01

--- 2022/01/run.py
+++ 2022/01/run.py
@@ -6,30 +6,34 @@ import helper
     Input
 ***************************************************************
 '''
-
 exampleInput = helper.getFileContentAsList("./example.txt")
 realInput = helper.getFileContentAsList("./input.txt")
-
-'''
-***************************************************************
-    Functions
-***************************************************************
-'''
-
 
 '''
 ***************************************************************
     Run
 ***************************************************************
 '''
-# What are we working with; Change this variable to use the real input when ready
-inputList = exampleInput
-print(inputList)
-
+# What are we working with
+inputList = realInput
+# print(inputList)
 
 # Print answer for Part 1
-helper.printAnswer(1,"Part 1 Default")
+elfVal = 0
+elfVals = []
+for x in inputList:
+    if x == "":
+        # max = elfVal if elfVal > max else max
+        elfVals.append(elfVal)
+        elfVal = 0
+    else:
+        elfVal += int(x)
+
+maxNum = max(elfVals)    
+helper.printAnswer(1, maxNum)
 
 
 # Print answer for Part 2
-helper.printAnswer(2, "Part 2 Default")
+elfVals.sort(reverse=True)
+maxNum = sum(elfVals[0:3])
+helper.printAnswer(2, maxNum)
