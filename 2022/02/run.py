e3729ccee3d8fd935072871c48cb9da7de171430
Derrick Fyfield <dlfyfield@smcm.edu>
2022-12-02 00:36:21 -0500

    Day 02

--- 2022/02/run.py
+++ 2022/02/run.py
@@ -15,6 +15,50 @@ realInput = helper.getFileContentAsList("./input.txt")
     Functions
 ***************************************************************
 '''
+# Rock (A, X) / Paper (B, Y) / Scissors (C, Z)
+def getScore(oppChoice, myChoice):
+    
+    choiceMap = {"A":1, "B":2, "C":3, "X":1, "Y":2, "Z":3}
+    choiceMapVal = {"A":"Rock", "B":"Paper", "C":"Scissors", "X":"Rock", "Y":"Paper", "Z":"Scissors"}
+    
+    oVal = choiceMap[oppChoice] #opponent 
+    mVal = choiceMap[myChoice] #mine
+
+    outcome = "Draw"
+    if oVal > mVal and (oVal != 1 and mVal != 3):
+        outcome = "Lose"
+    if oVal > mVal and (oVal == 3 and mVal == 1):
+        outcome = "Win"
+    elif oVal < mVal and (oVal == 1 and mVal == 3):
+        outcome = "Lose"
+    elif mVal > oVal:
+        outcome = "Win"
+
+    # print("{0} -- {1} == {2}".format(choiceMapVal[oppChoice], choiceMapVal[myChoice], outcome))
+    outcomeMap = {"Lose":0, "Draw":3, "Win":6}
+
+    return mVal + outcomeMap[outcome]
+
+
+# Rock (A) / Paper (B) / Scissors (C)
+# Lose (X) / Draw (Y) / Win (Z)
+def getStrategy(oppChoice, neededOutcome):
+    # Mapping what each letters (beats, loses to)
+    choiceMap = { 
+        "A":["C", "B"],
+        "B":["A", "C"],
+        "C":["B", "A"]
+    }
+    score = 0
+
+    if(neededOutcome == "X"):
+        score = getScore(oppChoice, choiceMap[oppChoice][0]) 
+    elif neededOutcome == "Y":
+        score = getScore(oppChoice, oppChoice)
+    elif neededOutcome == "Z":
+        score = getScore(oppChoice, choiceMap[oppChoice][1])
+    
+    return score
 
 
 '''
@@ -23,13 +67,16 @@ realInput = helper.getFileContentAsList("./input.txt")
 ***************************************************************
 '''
 # What are we working with; Change this variable to use the real input when ready
-inputList = exampleInput
-print(inputList)
-
+inputList = realInput
+# print(inputList)
 
 # Print answer for Part 1
-helper.printAnswer(1,"Part 1 Default")
+scores = [ getScore(x[0], x[1]) for x in (y.split(" ") for y in inputList)]
+a1 = sum(scores)
+helper.printAnswer(1,a1)
 
 
 # Print answer for Part 2
-helper.printAnswer(2, "Part 2 Default")
+scores = [ getStrategy(x[0], x[1]) for x in (y.split(" ") for y in inputList)]
+a2 = sum(scores)
+helper.printAnswer(2, a2)
