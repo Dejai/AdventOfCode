a2fd77d15048e00958e91994f31abf00d8c5c300
Derrick Fyfield <dlfyfield@smcm.edu>
2022-12-19 09:06:40 -0400

    Day 13

--- 2022/13/classes.py
+++ 2022/13/classes.py
@@ -1,122 +1,87 @@
 import json
 from collections import deque
 
+
+# Represents a single packet
 class Packet:
     def __init__ (self, items):
         self.Name = str(items)
-        self.Items = self.setItemsList(items)
-
+        self.Items = json.loads(items)
+        
     def __repr__(self):
-        # return "{0} --> {1}".format(self.Name, self.Items)
         return "{0}".format(self.Name)
 
-    def test(self):
-        print(self.test)
-        for x in self.test:
-            print(x)
+    # Compare this packet to another packet: Returns TRUE if less than, otherwise, FALSE
+    def compareTo(self, packet):
+        results = self.comparePackets(self.Items, packet.Items)
+        return results
 
-    def setItemsList(self, items):
-        itemsList = json.loads(items)
-        numbers = self.getNumbers(itemsList)
-        return [ int(x) for x in numbers.split(",") if x != ""]
+    # Evaluate if the packet pairs are in order
+    def comparePackets(self, left, right):
 
-    # Get the digit values from a packet
-    def getNumbers(self, inputList, level=1):
-        # If first element is a list & it is empty: return multiple -1 
-        if len(inputList) == 1:
-            if "list" in str(type(inputList[0])) and len(inputList[0]) == 0:
-                return "-1," * level
+        leftQueue = deque(self.toList(left))
+        rightQueue = deque(self.toList(right))
 
-        # If it is an empty list, return -1
-        if len(inputList) == 0:
-            return "-1,"
+        while True:
+            leftItem = leftQueue.popleft() if len(leftQueue) > 0 else None
+            rightItem = rightQueue.popleft() if len(rightQueue) > 0 else None
 
-        # Get list of values
-        values = ""
+            # print("Comparing: {0} ..... {1}".format(leftItem, rightItem))
 
-        for item in inputList:
-            if "int" in str(type(item)):
-                # print("Single Value = " + str(item))
-                values += str(item)+","
-            elif "list" in str(type(item)):
-                # print("List Value = " + str(item))
-                values += self.getNumbers(item, level+1)
+            # Get types
+            leftType = self.getType(leftItem)
+            rightType = self.getType(rightItem)
 
-        return values
+            # Check to see which lists run out ... and when 
+            if leftItem is None and rightItem is None:
+                return None 
+            elif leftItem is None and rightItem is not None:
+                return True
+            elif rightItem is None and leftItem is not None:
+                return False
 
+            # Compare values
+            if leftType == "INT" and rightType == "INT":
+                
+                if leftItem == rightItem:
+                    continue
+                elif leftItem < rightItem:
+                    return True
+                elif rightItem < leftItem:
+                    return False
+            else:
+                result = self.comparePackets(leftItem, rightItem)
+
+                if result is not None:
+                    return result
+                else:
+                    continue      
+
+    # Get the type of an item
+    def getType(self, value):
+
+        if "int" in str(type(value)):
+            return "INT"
+        return "LIST"
+
+    # Ensure every item is converted to a list for comparison
+    def toList(self, value):
+        if 'list' not in str(type(value)):
+            return [value]
+        else:
+            return value
+
+# Represents a pair of packet
 class Pair:
 
-    def __init__(self, index, packetPair):
+    def __init__(self, index, packet1, packet2):
+    # def __init__(self, index,packetPair):
         self.Index = index
-        self.RightOrder = False
-
-        # Create the packets
-        packets = packetPair.split("\n")
-        self.Left = Packet(packets[0])
-        self.Right = Packet(packets[1])
+        self.Left = packet1
+        self.Right = packet2
 
         # Evaluate packets
-        self.evaluatePair()
+        self.RightOrder = self.Left.compareTo(self.Right)
 
     def __repr__(self):
-        return "{0} = {1} / {2} \n\t{3}\n".format(self.Index, self.Left, self.Right, self.RightOrder) 
-
-    # Compare two packets to determine if they are in order
-    def evaluatePair(self):
-        self.RightOrder = True
-
-        # Get two pairs of items
-        leftQueue = deque(self.Left.Items)
-        rightQueue = deque(self.Right.Items)
-
-        idx = 0
-        while len(leftQueue) > 0 or len(rightQueue) > 0:
-
-            left = leftQueue.popleft() if len(leftQueue) > 0 else -1
-            right = rightQueue.popleft() if len(rightQueue) > 0 else -1
-
-            if len(leftQueue) == 0 and len(rightQueue) > 0:
-                self.RightOrder = True
-                return
-            elif len(leftQueue) > 0 and len(rightQueue) == 0:
-                self.RightOrder = False
-                return 
-            # print("Comparing {0} to {1}".format(left, right))
-
-            if left < right:
-                self.RightOrder = True
-                return 
-            
-            if right < left:
-                self.RightOrder = False
-                return        
-                
-    # def evaluatePair2(self, left, right):
-
-    #     leftList = self.toList(left)
-    #     rightList = self.toList(right)
-
-    #     if leftList[0].isnumeric() and rightList[0].isnumeric():
-    #         return leftList[0] < rightList[0]
-
-    #     while len(leftList) > 0 and len(rightList) > 0:
-    #         self.evaluatePair2(leftList[idx], rightList[idx])
-
-    #     if left is None and right is not None:
-    #         return False
-    #     elif left is not None and right is None:
-    #         return False 
-
-    #     leftQueue = deque(leftList)
-    #     rightQueue = deque(rightList)
-
-
-
-
-        
-    def toList(self, val):
-        if 'list' not in str(type(val)):
-            return list(val)
-        else:
-            return val
-        
+        return "{0} = {1} / {2} \n\t{3}\n".format(self.Index, self.Left, self.Right, self.RightOrder)
--- 2022/13/run.py
+++ 2022/13/run.py
@@ -1,6 +1,6 @@
 
 import helper
-from classes import Pair
+from classes import Packet,Pair
 
 '''
 ***************************************************************
@@ -10,12 +10,55 @@ from classes import Pair
 
 exampleInput = helper.getFileContentAsList("./example.txt")
 realInput = helper.getFileContentAsList("./input.txt")
+testInput = helper.getFileContentAsList("./inputTest.txt")
 
 '''
 ***************************************************************
     Functions
 ***************************************************************
 '''
+
+# Get the packets & Pairs
+def getPacketDetails(inptuList):
+
+    pairs = []
+    packets = []
+
+    for idx in range(0, len(inputList)):
+
+        packetPair = inputList[idx]
+        packetCouple = packetPair.split("\n")
+        packet1 = Packet(packetCouple[0])
+        packet2 = Packet(packetCouple[1])
+        pair = Pair(idx+1, packet1, packet2)
+
+        packets.append(packet1)
+        packets.append(packet2)
+        pairs.append(pair)
+
+    return packets,pairs
+
+
+# Sort the packets
+def sortPackets(packetList):
+    print("Sorting packets ..... ")
+    idx = 0
+    while True:
+        if idx == len(packetList)-1:
+            packetNames = [ p.Name for p in packetList ]
+            return packetNames
+
+        p1 = packetList[idx]
+        p2 = packetList[idx+1]
+        
+        if p1.compareTo(p2):
+            idx += 1
+            continue
+        else:
+            temp = packetList[idx+1]
+            packetList[idx+1] = packetList[idx]
+            packetList[idx] = temp
+            idx = 0 
 
 
 '''
@@ -25,29 +68,27 @@ realInput = helper.getFileContentAsList("./input.txt")
 '''
 
 # What are we working with; Change this variable to use the real input when ready
-inputList = exampleInput
+inputList = realInput
 # print(inputList)
 
 # Print answer for Part 1
-pairs = []
-for idx in range(0, len(inputList)):
-    pair = Pair(idx+1,inputList[idx])
-    pairs.append(pair)
-    print(pair)
+packets,pairs = getPacketDetails(inputList)
 a1 = sum([p.Index for p in pairs if p.RightOrder == True])
 helper.printAnswer(1,a1)
 
-falses = [p for p in pairs if p.RightOrder == False]
-file = open("output.txt", "w+")
-for f in falses:
-    file.write("{0}\n".format(f))
-    file.write("\t{0}\n\t{1}".format(f.Left.Items, f.Right.Items))
-    file.write("\n\n")
-file.close()
-
-# Too low: 5394
-# Too high: 5928
-
-
 # Print answer for Part 2
-# helper.printAnswer(2, "Part 2 Default")
+runPt2 = input("Do you want to run part 2 (it takes a little while)? (y/n): ")
+if runPt2 == "y":
+    # Add the two new packets
+    packet1,packet2 = [ "[[2]]", "[[6]]" ]
+    packets.append( Packet(packet1) )
+    packets.append( Packet(packet2) )
+
+    # Sort the packets
+    sortedPacketNames = sortPackets(packets)
+
+    # Get index of dividers
+    idx1, idx2 = [ sortedPacketNames.index(packet1)+1, sortedPacketNames.index(packet2)+1 ]
+    a2 = idx1 * idx2
+    helper.printAnswer(2, a2)
+    
