557b71264e61c8255ada1c4ecd671ef285d31fcd
Derrick Fyfield <dancinglion.dev@gmail.com>
2023-12-08 04:26:05 -0500

    prod 2023 Day 07 (cleanup)

--- 2023/07/run.py
+++ 2023/07/run.py
@@ -14,101 +14,84 @@ inputList = helper.getFileContentAsList("./input.txt")
     Functions
 ***************************************************************
 '''
-def getHandRank(hand):
-    rankings = ["[1, 1, 1, 1, 1]", "[2, 1, 1, 1]", "[2, 2, 1]", "[3, 1, 1]", "[3, 2]", "[4, 1]", "[5]"]
-    freq = {}
-    for card in list(hand):
-        if card in freq:
-            freq[card] += 1
-        else:
-            freq[card] = 1
-    freqVals = list(freq.values())
-    freqVals.sort(reverse=True)
-    freqKey = str(freqVals)
-    idx = rankings.index(freqKey)
-    return idx
+class CardSorter: 
+    def __init__(self):
+        self.Cards = list('23456789TJQKA')
+        self.Rankings = ["[1, 1, 1, 1, 1]", "[2, 1, 1, 1]", "[2, 2, 1]", "[3, 1, 1]", "[3, 2]", "[4, 1]", "[5]"]
+        self.Joker = False
 
-def compareHands(pair1, pair2):
-    hand1 = pair1[0]
-    hand2 = pair2[0]
-
-    hand1Rank = getHandRank(hand1)
-    hand2Rank = getHandRank(hand2)
-    if hand1Rank < hand2Rank:
-        return -1
-    elif hand1Rank > hand2Rank:
-        return 1
-    else:
-        cards = list('23456789TJQKA')
-        for idx in range(0, len(hand1)):
-            h1_idx = cards.index(hand1[idx])
-            h2_idx = cards.index(hand2[idx])
-            if h1_idx < h2_idx:
-                return -1
-            elif h1_idx > h2_idx:
-                return 1
-        return 0
+    # Set whether the Joker variant is in effect
+    def setJoker(self):
+        self.Joker = True
+        self.Cards = list('J23456789TQKA')
     
-def getHandRankJoker(hand):
-    rankings = ["[1, 1, 1, 1, 1]", "[2, 1, 1, 1]", "[2, 2, 1]", "[3, 1, 1]", "[3, 2]", "[4, 1]", "[5]"]
-    freq = {}
-    highestFreq = 0;
-    highestCardFreq = ""
-    for card in list(hand):
-        if card not in freq:
-            freq[card] = 0
-        freq[card] += 1
-        if freq[card] > highestFreq and card != "J":
-            highestFreq = freq[card]
-            highestCardFreq = card
+    # Get the rank of a single hand (based on frequency rankings)
+    def getHandRank(self, hand):
+        freq = {}
+        highestFreq = 0
+        highestCardFreq = ""
+        for card in list(hand):
+            if card not in freq:
+                freq[card] = 0
+            freq[card] += 1
+            # Only bother with highest frequency if doing the Joker variant
+            if self.Joker and freq[card] > highestFreq and card != "J":
+                highestFreq = freq[card]
+                highestCardFreq = card
 
-    # Convert Js to te best other option & then remove
-    if "J" in freq and highestCardFreq != "":
-        freq[highestCardFreq] += freq["J"]
-        del freq["J"]
+        # Convert Js to te best other option & then remove (for Joker variant)
+        if self.Joker and "J" in freq and highestCardFreq != "":
+            freq[highestCardFreq] += freq["J"]
+            del freq["J"]
 
-    freqVals = list(freq.values())
-    freqVals.sort(reverse=True)
-    freqKey = str(freqVals)
-    idx = rankings.index(freqKey)
-    return idx
+        freqVals = list(freq.values())
+        freqVals.sort(reverse=True)
+        freqKey = str(freqVals)
+        idx = self.Rankings.index(freqKey)
+        return idx
 
-def compareHandsJoker(pair1, pair2):
-    hand1 = pair1[0]
-    hand2 = pair2[0]
+    def compareHands(self, pair1, pair2):
+        hand1 = pair1[0]
+        hand2 = pair2[0]
 
-    hand1Rank = getHandRankJoker(hand1)
-    hand2Rank = getHandRankJoker(hand2)
-    if hand1Rank < hand2Rank:
-        return -1
-    elif hand1Rank > hand2Rank:
-        return 1
-    else:
-        cards = list('J23456789TQKA')
-        for idx in range(0, len(hand1)):
-            h1_idx = cards.index(hand1[idx])
-            h2_idx = cards.index(hand2[idx])
-            if h1_idx < h2_idx:
-                return -1
-            elif h1_idx > h2_idx:
-                return 1
-        return 0
+        hand1Rank = self.getHandRank(hand1)
+        hand2Rank = self.getHandRank(hand2)
+        if hand1Rank < hand2Rank:
+            return -1
+        elif hand1Rank > hand2Rank:
+            return 1
+        else:
+            cards = list('23456789TJQKA')
+            for idx in range(0, len(hand1)):
+                h1_idx = self.Cards.index(hand1[idx])
+                h2_idx = self.Cards.index(hand2[idx])
+                if h1_idx < h2_idx:
+                    return -1
+                elif h1_idx > h2_idx:
+                    return 1
+            return 0
+        
+    def sortCards(self, pairs):
+        pairsSorted = sorted(pairs, key=cmp_to_key(self.compareHands))
+        return pairsSorted
 
 '''
 ***************************************************************
     Run
 ***************************************************************
 '''
+pairs = [ (card,int(bid) ) for card,bid in [x.split(" ") for x in inputList ] ]
+cardSorter = CardSorter()
 
 # Print answer for Part 1
-pairs = [ (card,int(bid) ) for card,bid in [x.split(" ") for x in inputList ] ]
-pairsSorted = sorted(pairs, key=cmp_to_key(compareHands))
+pairsSorted = cardSorter.sortCards(pairs)
 winnings = [ (pair[1] * (idx+1) ) for idx,pair in enumerate(pairsSorted) ]
 ans1 = sum(winnings)
 helper.printAnswer(1,ans1)
 
 # Print answer for Part 2
-pairsSorted2 = sorted(pairs, key=cmp_to_key(compareHandsJoker))
+cardSorter.setJoker()
+pairsSorted2 = cardSorter.sortCards(pairs)
 winnings2 = [ (pair[1] * (idx+1) ) for idx,pair in enumerate(pairsSorted2) ]
 ans2 = sum(winnings2)
 helper.printAnswer(2, ans2)
